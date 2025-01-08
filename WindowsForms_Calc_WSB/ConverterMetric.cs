d8401f1e1d58f7055a665aa01347024fc911d3a1
filmosz_MTV <filip.moszczynski@mtv.fi>
2025-01-08 21:50:16 +0100

    ConverterMetric class added

---
+++ WindowsForms_Calc_WSB/ConverterMetric.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms_Calc_WSB
+{
+    internal class ConverterMetric
+    {
+        /*
+         Object contining methods returning values in SI units
+         */
+
+        double foot = 30.48;
+        double pound = 0.45359237;
+        double grain = 0.06479891;
+        double spine = 1; //find correct ratio
+
+        public ConverterMetric() { }
+
+        double poundage(double pounds, int round)
+        {
+            return Math.Round(pounds / pound, round);
+        }
+
+        double weight(double grains, int round)
+        {
+            return Math.Round(grains / grain, round);
+        }
+
+        double speed(double feet, int round)
+        {
+            return Math.Round(feet / foot, round);
+        }
+
+        double staticSpine(double tradSpine, int round) //may require improvement
+        {
+            return tradSpine / spine;
+        }
+    }
+}
